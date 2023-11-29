1ceff8743d347ee117778b837a3c1d29bb145315
MikaelEngvall <mikael@engvall.org>
2023-11-29 16:39:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/header/Header';
+import { AdsList } from './components/AdsList';
+
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+            <AdsList />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/Advertisement.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+
+const Advertisement = (props) => {
+  const [showContact, setShowContact] = useState(false);
+
+  const handleContactClick = () => {
+    if (showContact) {
+      // If currently showing contact, hide it
+      setShowContact(false);
+    } else {
+      // If currently hiding contact, show it
+      setShowContact(true);
+    }
+  };
+    return (
+        <div className="card mb-4 col-md-4">
+        <img
+          className="card-img-top"
+          src={props.picture}
+          alt="Ad Title"
+        />
+        <div className="card-body">
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.description}</p>
+          <p className="card-text" onClick={handleContactClick}>  
+          Contact: {showContact ? props.contact : '***'}
+          </p>
+        </div>
+        <div className="card-footer text-center">
+          <button
+            className="btn btn-info btn-block"
+            onClick={() => alert(`Title: ${props.contact}\nDescription: ${props.description}\nContact ${props.contact}`)}
+          >
+            Details
+          </button>
+        </div>
+      </div>
+    );
+};
+
+export default Advertisement;
